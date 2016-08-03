4293ad4ce749230a0146abb0f4f79907abc98f6c
Mateusz Korzel <mkorzel@objectivity.co.uk>
2016-08-03 09:56:10 +0200

    Small fix for polyfills loading in amd format.

--- src/oauth-polyfills.ts
+++ src/oauth-polyfills.ts
@@ -1,19 +1,13 @@
-export function objectAssign(target, ...args: any[]) {
-    if (target == null) {
+export function objectAssign(target, source) {
+    if (target == null || source == null) {
         throw new TypeError('Cannot convert undefined or null to object');
     }
 
     target = Object(target);
 
-    for (var index = 1; index < args.length; index++) {
-        var source = args[index];
-
-        if (source != null) {
-            for (var key in source) {
-                if (Object.prototype.hasOwnProperty.call(source, key)) {
-                    target[key] = source[key];
-                }
-            }
+    for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
         }
     }
 
